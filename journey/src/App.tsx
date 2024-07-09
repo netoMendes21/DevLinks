6e68cc3253157c4fe7afecf540b574c8ef8ff517
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2024-07-09 11:38:11 -0300

    iniciando projeto

---
+++ journey/src/App.tsx
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <div>
+      <p>
+        Convide seus amigos e planeje sua próxima viagem!
+        <p>
+          Ao planejar sua viagem pela plann.er você automaticamente concorda com
+          nossos <a href="#">termos de uso</a>e
+          <a href="#">políticas de privacidade.</a>
+        </p>
+      </p>
+    </div>
+  );
+}
