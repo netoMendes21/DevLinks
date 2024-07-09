decd1908f86f29b2b947eb7763713bfd1f77dc09
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2024-07-09 20:49:51 -0300

    tela home pronta , sem reações ainda

--- journey/src/App.tsx
+++ journey/src/App.tsx
@@ -1,27 +1,52 @@
+import { MapPin, Calendar, ArrowRight } from "lucide-react";
+
 export function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center bg-quadrado bg-pattern bg-no-repeat bg-center">
       <div className="max-w-full px-6 text-center space-y-10">
+
+        <div className="flex flex-col items-center gap-3">
+        <img src="/logo.svg" alt="plann.er" />
         <p className="text-zinc-300 text-lg">
           Convide seus amigos e planeje sua próxima viagem!
-      <div>
-
-        <div className="h-16  bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
-          <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400"/>
-          <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400"/>
-
-          <button>Continuar</button>
+        </p>
         </div>
         
-      </div>
-          <p className="text-sm texe-zinc-500">
-            Ao planejar sua viagem pela plann.er você automaticamente concorda com
-          nossos <a className="text-zinc-300 underline" href="#">termos de uso</a>e
-          <a className="text-zinc-300 underline" href="#">políticas de privacidade.</a>
+        <div>
+          <div className="h-16  bg-zinc-900 px-4 rounded-xl flex gap-3 items-center shadow-shape">
+            <div className="flex items-center gap-2 flex-1">
+              <MapPin className="size-5 text-zinc-400" />
+              <input
+                type="text"
+                placeholder="Para onde você vai?"
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none"
+              />
+            </div>
+
+            <div className="flex items-center gap-2">
+              <Calendar className="size-5 text-zinc-400" />
+              <input
+                type="text"
+                placeholder="Quando?"
+                className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"
+              />
+            </div>
+
+            <div className="w-px h-6 bg-zinc-800"></div>
+
+            <button className="bg-lime-300 text-lime-950 flex items-center gap-2 rounded-lg px-5 py-2 font-medium hover:bg-lime-400">
+              Continuar <ArrowRight className="size-5" />
+            </button>
+          </div>
+        </div>
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+          com nossos <a className="text-zinc-300 underline" href="#">termos de uso
+          </a> e <a className="text-zinc-300  underline" href="#">
+            políticas de privacidade.
+          </a>
         </p>
-      </p>
+      </div>
     </div>
-    </div>
-    
   );
 }
